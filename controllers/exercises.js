e652b8fa57a4ae31d46cbf2637e8c87f6112afb2
Kevin Loughead <kvnloughead@gmail.com>
2021-10-02 18:31:31 -0400

    fix: addExercise returns user instance

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -12,35 +12,14 @@ const addExercise = (req, res) => {
         .then((user) => {
           user.logs.push(exercise._id);
           user.save();
+          res.send({ _id: user._id, username: user.username, description, date: parseDate(date), duration })
         })
         .catch((err) => console.log(err));
       return exercise;
     })
-    .then((exercise) => {
-      Exercise.populate(exercise, { path: 'userId' })
-        .then((exercise) => {
-          const { userId: user, description, date, duration } = exercise;
-          const result = { 
-            description, 
-            _id: user._id, 
-            username: user.username, 
-            date: parseDate(date), 
-            duration: parseFloat(duration) 
-          }
-          res.send(result);
-        })
-    })  
     .catch((err) => {
       res.send(err);
     });
 }
-
-// const getLogs = (req, res) => {
-//   const { id: userId } = req.params;
-//   const { from, to, limit } = req.query;
-//   Exercise.find({ userId })
-//     .then((result) => res.send(result))
-//     .catch((err) => console.log(err));
-// }
 
 module.exports = { addExercise };
