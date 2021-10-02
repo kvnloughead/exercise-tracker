9339398bf37474706eb27b1d4e84ed388dba3b53
Kevin Loughead <kvnloughead@gmail.com>
2021-10-02 18:56:45 -0400

    fix: format of datestring

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,9 @@
 exports.parseDate = (date) => {
   // date must be of type Date
   date = date instanceof Date ? date : new Date(date); 
-  let parsedDate = date.toLocaleDateString("en-US", 
-    { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' }
-  );
-  return parsedDate.replace(/,/g, '');
+  // let parsedDate = date.toLocaleDateString("en-US", 
+  //   { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' }
+  // );
+  // return parsedDate.replace(/,/g, '');
+  return date.toDateString()
 }
